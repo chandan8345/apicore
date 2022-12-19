3dd1b057a87ba699b3023020ca76f26ad050f8d3
chandan kumar <chandan8345@gmail.com>
2022-12-19 07:44:30 -0800

    api ready with stored procedure

---
+++ Mafia/Mafia/Controllers/TestController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Mafia.DAL;
+using System.Collections;
+using Mafia.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Mafia.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        data_access da = new data_access();
+        // GET: api/<TestController>
+        [HttpGet]
+        public string Get()
+        {
+            string sql = "select * from EmployeeAlt";
+            DataTable dt = da.GetDataTableByCommand(sql);
+            return JsonConvert.SerializeObject(dt);
+        }
+
+        // GET api/<apiController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            string sql = "select * from EmployeeAlt where id='" + id + "'";
+            DataTable dt = da.GetDataTableByCommand(sql);
+            return JsonConvert.SerializeObject(dt);
+        }
+
+        // POST api/<apiController>
+        [HttpPost]
+        public string Post([FromBody] Empolyee empolyee)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("FirstName", empolyee.FirstName);
+            DataTable dt = da.ExecuteStoredProcedure("insert_empoloyee", ht);
+            return JsonConvert.SerializeObject(dt);
+        }
+
+        // PUT api/<apiController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+            
+        }
+
+        // DELETE api/<apiController>/5
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            string sql = "delete from EmployeeAlt where id='" + id + "'";
+            da.ExecuteScalar(sql);
+            return "delete success";
+        }
+    }
+}
---
+++ Mafia/Mafia/Models/Empolyee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mafia.Models
+{
+    public class Empolyee
+    {
+        public string FirstName { get; set; }
+    }
+}
